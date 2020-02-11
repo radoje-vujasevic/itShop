5beda33e88c0219fec91a3068f369e00af578c4d
Radoje Vujasevic <radojevujasevic@gmail.com>
2020-02-11 11:34:32 +0100

    Initial Commit

---
+++ itShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itShop
+//
+//  Created by Raša on 11/02/2020.
+//  Copyright © 2020 Rasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
