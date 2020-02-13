18bb18c4d30ba83c44d453b551dd0863741e7428
Radoje Vujasevic <radojevujasevic@gmail.com>
2020-02-13 14:38:03 +0100

    Things that have been done: - Moved logic from SubCategoryController to CategoryViewController - Deleted SubCategoryViewController - Now as long as there are categories, CategoryVC is going to call itself until it hits ItemList

--- itShop/Controllers/CategoriesViewController.swift
+++ itShop/Controllers/CategoriesViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class CategoriesViewController: UITableViewController {
     var categories:[Category] = []
+    var start: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        loadCategories()
+        if (start){
+            loadCategories()
+        }
     }
-    
+        
     //MARK: - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
@@ -30,13 +33,30 @@ class CategoriesViewController: UITableViewController {
     
     //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "segueToSubCategories", sender: self)
+        // If there are children categories and there is more than one child category --> Push to navigation controller this ViewController as next view
+        if (categories[indexPath.row].ChildsExist && categories[indexPath.row].ChildCategories.count > 1){
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let loadVC = storyBoard.instantiateViewController(withIdentifier: "CategoryList") as! CategoriesViewController
+            loadVC.categories = categories[indexPath.row].ChildCategories
+            loadVC.start = false
+            self.navigationController?.pushViewController(loadVC, animated: true)
+        }
+        else {
+            performSegue(withIdentifier: "segueToItemsList", sender: self)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let indexPath = tableView.indexPathForSelectedRow {
-            let destinationVC = segue.destination as! SubCategoriesViewController
-            destinationVC.childCategories = categories[indexPath.row].ChildCategories
+            if let destinationVC = segue.destination as? ItemListViewController{
+                let clickedCategory = categories[indexPath.row]
+                if(clickedCategory.ChildCategories.count == 1){
+                    destinationVC.categoryID = clickedCategory.ChildCategories[0].Id
+                }
+                else{
+                    destinationVC.categoryID = clickedCategory.Id
+                }
+            }
         }
     }
     
